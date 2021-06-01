19dc33b32b66437d2b87b8b5059e9a132362db4b
EagleByte <28986398+TheEagleByte@users.noreply.github.com>
2021-06-01 17:57:21 -0400

    Update CollectionExtensions.cs

--- CollectionExtensions.cs
+++ CollectionExtensions.cs
@@ -1,26 +1,43 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography;
 
 namespace GreymanRaffleWinnerSelector
 {
     public static class CollectionExtensions
     {
+
         public static void Shuffle<T>(this IList<T> list)
         {
-            var provider = new RNGCryptoServiceProvider();
-            var n = list.Count;
+            var rng = new Random();
+            int n = list.Count;
             while (n > 1)
             {
-                var box = new byte[1];
-                do provider.GetBytes(box);
-                while (!(box[0] < n * (byte.MaxValue / n)));
-                var k = box[0] % n;
                 n--;
-                var value = list[k];
+                int k = rng.Next(n + 1);
+                T value = list[k];
                 list[k] = list[n];
                 list[n] = value;
             }
         }
+
+        //public static void Shuffle<T>(this IList<T> list)
+        //{
+        //    var provider = new RNGCryptoServiceProvider();
+        //    var n = list.Count;
+        //    while (n > 1)
+        //    {
+        //        var box = new byte[1];
+        //        do provider.GetBytes(box);
+        //        while (!(box[0] < n * (byte.MaxValue / n)));
+        //        var k = box[0] % n;
+        //        n--;
+        //        var value = list[k];
+        //        list[k] = list[n];
+        //        list[n] = value;
+        //    }
+        //}
 
         public static T SelectRandom<T>(this IList<T> list)
         {
